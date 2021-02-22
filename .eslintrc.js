db96ff7e70b3bac744f9562c0af10a5c306f9608
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-02-22 15:25:21 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 11
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
