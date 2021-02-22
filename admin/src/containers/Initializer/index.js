eabbd37eeaa9e7184f739b63fc382cf05085f8a1
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-02-22 15:36:13 +0100

    Fix stylin on plugin

--- admin/src/containers/Initializer/index.js
+++ admin/src/containers/Initializer/index.js
@@ -4,23 +4,23 @@
  *
  */
 
-import { useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+import { useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
+import pluginId from '../../pluginId'
 
 const Initializer = ({ updatePlugin }) => {
-  const ref = useRef();
-  ref.current = updatePlugin;
+  const ref = useRef()
+  ref.current = updatePlugin
 
   useEffect(() => {
-    ref.current(pluginId, 'isReady', true);
-  }, []);
+    ref.current(pluginId, 'isReady', true)
+  }, [])
 
-  return null;
-};
+  return null
+}
 
 Initializer.propTypes = {
-  updatePlugin: PropTypes.func.isRequired,
-};
+  updatePlugin: PropTypes.func.isRequired
+}
 
-export default Initializer;
+export default Initializer
--- admin/src/translations/index.js
+++ admin/src/translations/index.js
@@ -1,25 +1,25 @@
-import ar from './ar.json';
-import cs from './cs.json';
-import de from './de.json';
-import en from './en.json';
-import es from './es.json';
-import fr from './fr.json';
-import id from './id.json';
-import it from './it.json';
-import ko from './ko.json';
-import ms from './ms.json';
-import nl from './nl.json';
-import pl from './pl.json';
-import ptBR from './pt-BR.json';
-import pt from './pt.json';
-import ru from './ru.json';
-import th from './th.json';
-import tr from './tr.json';
-import uk from './uk.json';
-import vi from './vi.json';
-import zhHans from './zh-Hans.json';
-import zh from './zh.json';
-import sk from './sk.json';
+import ar from './ar.json'
+import cs from './cs.json'
+import de from './de.json'
+import en from './en.json'
+import es from './es.json'
+import fr from './fr.json'
+import id from './id.json'
+import it from './it.json'
+import ko from './ko.json'
+import ms from './ms.json'
+import nl from './nl.json'
+import pl from './pl.json'
+import ptBR from './pt-BR.json'
+import pt from './pt.json'
+import ru from './ru.json'
+import th from './th.json'
+import tr from './tr.json'
+import uk from './uk.json'
+import vi from './vi.json'
+import zhHans from './zh-Hans.json'
+import zh from './zh.json'
+import sk from './sk.json'
 
 const trads = {
   ar,
@@ -43,7 +43,7 @@ const trads = {
   vi,
   'zh-Hans': zhHans,
   zh,
-  sk,
-};
+  sk
+}
 
-export default trads;
+export default trads
