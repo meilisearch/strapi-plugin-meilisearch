32df851722fe7156aba3fac82014e722e1abe119
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-02-25 17:04:13 +0100

    Store MeiliSearch Credentials in DB

--- controllers/meilisearch.js
+++ controllers/meilisearch.js
@@ -6,46 +6,10 @@
  * @description: A set of functions called "actions" of the `meilisearch` plugin.
  */
 
+const credentialsService = () => strapi.plugins.meilisearch.services.credentials_store
+const meiliSearchService = () => strapi.plugins.meilisearch.services.meilisearch
+
 module.exports = {
-
-  /**
-   * Default action.
-   *
-   * @return {Object}
-   */
-  index: async (ctx) => {
-    // Add your own logic here.
-    // Send 200 `ok`
-    ctx.send({
-      message: 'ok'
-    })
-  },
-
-  /**
-   * Get Or Create Index in MeilISearch
-   *
-   * @return {Object}
-   */
-  getOrCreateIndex: async (ctx) => {
-    const meiliSearchService = strapi.plugins.meilisearch.services.meilisearch
-    console.log(meiliSearchService)
-    console.log(ctx.request.body)
-    const { uid } = ctx.request.body
-    const index = await meiliSearchService.getOrCreateIndex(uid)
-    ctx.send(index)
-  },
-
-  /**
-   * Delete Index in MeilISearch
-   *
-   * @return {Object}
-   */
-  deleteIndex: async (ctx) => {
-    const meiliSearchService = strapi.plugins.meilisearch.services.meilisearch
-    const { indexUid } = ctx.params
-    await meiliSearchService.deleteIndex(indexUid)
-    ctx.send({ removed: true })
-  },
 
   /**
    * Add documents and Update in MeiliSearch
@@ -53,24 +17,44 @@ module.exports = {
    * @return {Object}
    */
   addDocuments: async (ctx) => {
-    const meiliSearchService = strapi.plugins.meilisearch.services.meilisearch
     const { indexUid } = ctx.params
     const { data } = ctx.request.body
-    const updateId = await meiliSearchService.addDocuments(indexUid, data)
-    ctx.send(updateId)
+    const { apiKey, host } = await credentialsService().getMeiliSearchCredentials()
+    try {
+      const updateId = await meiliSearchService().addDocuments({
+        apiKey,
+        host
+      }, indexUid, data)
+      ctx.send(updateId)
+    } catch (e) {
+      ctx.send({
+        error: true,
+        message: `${e.type}: \n${e.message || e.code}`,
+        ...(e.errorLink ? { link: e.errorLink } : {})
+      })
+    }
   },
-
-  /**
-   * Delete Documents in MeiliSearch
-   *
-   * @return {Object}
-   */
-  deleteDocuments: async (ctx) => {
-    const meiliSearchService = strapi.plugins.meilisearch.services.meilisearch
-    const { indexUid } = ctx.params
-    const { data } = ctx.request.body
-    await meiliSearchService.addDocuments(indexUid, data)
-    ctx.send({ message: 'ok' })
+  getMeiliSearchCredentials: async (ctx) => {
+    const { apiKey, host } = await credentialsService().getMeiliSearchCredentials()
+    ctx.send({
+      apiKey,
+      host
+    })
+  },
+  addMeiliSearchCredentials: async (ctx) => {
+    const { host: msHost, apiKey: msApiKey } = ctx.request.body
+    await credentialsService().setStoreKey({
+      key: 'meilisearchApiKey',
+      value: msApiKey
+    })
+    await credentialsService().setStoreKey({
+      key: 'meilisearchHost',
+      value: msHost
+    })
+    const { apiKey, host } = await credentialsService().getMeiliSearchCredentials()
+    ctx.send({
+      apiKey,
+      host
+    })
   }
-
 }
--- services/meilisearch.js
+++ services/meilisearch.js
@@ -2,11 +2,11 @@
 
 const { MeiliSearch } = require('meilisearch')
 
-const config = {
-  host: 'http://127.0.0.1:7700',
-  apiKey: 'masterKey'
-}
-const client = new MeiliSearch(config)
+// const config = {
+//   host: 'http://127.0.0.1:7700',
+//   apiKey: 'masterKey'
+// }
+const getClient = (config) => new MeiliSearch(config)
 
 /**
  * meilisearch.js service
@@ -15,16 +15,8 @@ const client = new MeiliSearch(config)
  */
 
 module.exports = {
-  async getOrCreateIndex (indexUid) {
-    return client.getOrCreateIndex(indexUid)
-  },
-  async deleteIndex (indexUid) {
-    return client.deleteIndex(indexUid)
-  },
-  async addDocuments (indexUid, documents) {
+  async addDocuments (config, indexUid, documents) {
+    const client = getClient(config)
     return client.index(indexUid).addDocuments(documents)
-  },
-  async deleteDocuments (indexUid, documentsId) {
-    return client.index(indexUid).deleteDocuments(documentsId)
   }
 }
