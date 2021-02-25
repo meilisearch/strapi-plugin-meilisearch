ffc4f8a7cf3ff9e783c41efd17a9e7a41a346802
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-02-25 17:03:15 +0100

    Add UI elements

--- admin/src/containers/HomePage/index.js
+++ admin/src/containers/HomePage/index.js
@@ -4,78 +4,129 @@
  *
  */
 
-import React, { memo, useState } from 'react'
+import React, { memo, useState, useEffect } from 'react'
 import { request, useGlobalContext } from 'strapi-helper-plugin'
 // import PropTypes from 'prop-types';
 import styled from 'styled-components'
 import pluginId from '../../pluginId'
 import { Button, InputText, Label } from '@buffetjs/core'
 import { Header } from '@buffetjs/custom'
+import getTrad from '../../utils/getTrad'
 
-const getTrad = (id) => `${pluginId}.${id}`
 const Wrapper = styled.div`
   margin-bottom: 30px;
 `
 
-// [
-//   {
-//     "id": 1,
-//     "title": "batman"
-//   },
-//   {
-//     "id": 2,
-//     "title": "test"
-//   }
-// ]
-
 const HomePage = () => {
   const [indexUid, setIndexUid] = useState('')
+  const [msApiKey, setApiKey] = useState('')
+  const [msHost, setHost] = useState('')
   const [documents, setDocuments] = useState('')
   const { formatMessage } = useGlobalContext()
+  useEffect(() => {
+    strapi.lockApp()
+    async function autoFillMsCredentials () {
+      const { apiKey } = await getMeiliSearchCredentials()
+      console.log(apiKey)
+      if (apiKey) setApiKey('*******')
+    }
+    autoFillMsCredentials()
+    strapi.unlockApp()
+  }, [])
 
   const addDocuments = async () => {
     strapi.lockApp()
-    await request(`/${pluginId}/index/${indexUid}/documents`, {
+    const res = await request(`/${pluginId}/index/${indexUid}/documents`, {
       method: 'POST',
       body: {
         data: JSON.parse(documents)
       }
     })
+    if (res.error) {
+      strapi.notification.toggle({
+        title: 'Operation on MeiliSearch failed',
+        type: 'warning',
+        message: res.message,
+        ...(res.link ? { link: { url: res.link, label: 'more information' } } : {}),
+        timeout: 4000
+      })
+    }
     strapi.unlockApp()
+  }
+  const getMeiliSearchCredentials = async () => {
+    const { apiKey, host } = await request(`/${pluginId}/credentials/`, {
+      method: 'GET'
+    })
+    console.log({ apiKey, host })
+    setHost(host)
+    return { apiKey, host }
+  }
+
+  const addMeilisearchCredentials = async () => {
+    console.log({ msHost, msApiKey })
+    const { apiKey, host } = await request(`/${pluginId}/credentials/`, {
+      method: 'POST',
+      body: {
+        host: msHost,
+        apiKey: msApiKey
+      }
+    })
+    if (apiKey) setApiKey('*******')
+    setHost(host)
   }
 
   return (
     <div className="container-fluid" style={{ padding: '18px 30px 66px 30px' }}>
       <Header
-        actions={[
-          {
-            label: 'test',
-            onClick: () => console.log('test'),
-            color: 'primary',
-            type: 'button',
-            icon: true
-          }
-        ]}
         title={{
           label: formatMessage({ id: getTrad('plugin.name') })
         }}
         content={formatMessage({ id: getTrad('header.description') })}
       />
       <Wrapper>
-        <Label htmlFor="input" message="Index name" />
+        <Label htmlFor="MSHost" message="MeiliSearch Host" />
         <InputText
-          name="input"
+          name="MSHost"
+          onChange={({ target: { value } }) => {
+            setHost(value)
+          }}
+          placeholder="Indexname"
+          type="text"
+          value={msHost}
+        />
+
+        <Label htmlFor="MSApiKey" message="MeiliSearch Api Key" />
+        <InputText
+          name="MSApiKey"
+          onChange={({ target: { value } }) => {
+            setApiKey(value)
+          }}
+          placeholder="documents"
+          type="text"
+          value={msApiKey}
+        />
+     </Wrapper>
+      <Button onClick={getMeiliSearchCredentials}>
+        store
+      </Button>
+      <Button onClick={addMeilisearchCredentials}>
+        Add
+      </Button>
+      <Wrapper>
+        <Label htmlFor="indexName" message="Index name" />
+        <InputText
+          name="indexName"
           onChange={({ target: { value } }) => {
             setIndexUid(value)
           }}
-          placeholder="Indexname"
+          placeholder="index name"
           type="text"
           value={indexUid}
         />
 
-        <Label htmlFor="input" message="Documents in JSON string" />
+        <Label htmlFor="documents" message="Documents in JSON string" />
         <InputText
-          name="input"
+          name="documents"
           onChange={({ target: { value } }) => {
             setDocuments(value)
           }}
