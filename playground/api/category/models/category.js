443c21136c3b205742d54ebd3364528534356df6
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-03-16 15:51:32 +0100

    Remove category hook

--- playground/api/category/models/category.js
+++ playground/api/category/models/category.js
@@ -1,32 +1,11 @@
 'use strict'
 
-const fs = require('fs')
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-const meilisearch = {
-  http: () => strapi.plugins.meilisearch.services.meilisearch_http,
-  client: (credentials) => strapi.plugins.meilisearch.services.meilisearch_client(credentials),
-  store: () => strapi.plugins.meilisearch.services.plugin_store('meilisearchCredentials')
-}
-
 module.exports = {
   lifecycles: {
-    async beforeFindOne (params, data) {
-      // const apiKey = await meilisearch.store().getStoreKey('meilisearchApiKey')
-      // const host = await meilisearch.store().getStoreKey('meilisearchHost')
-      console.log({ apiKey, host })
-    },
-    async beforeFind (params, data) {
-      console.log({ params, data })
-      console.log(strapi.plugins.meilisearch.services.meilisearch)
-    },
-    async beforeSearch (params, data) {
-      console.log({ params, data })
-      console.log(strapi.plugins.meilisearch.services.meilisearch)
-    }
   }
-
 }
