88e9c93f2a62a59b734125ad3768d1937a762237
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-06-22 16:09:18 +0200

    Handle credentials check in backend

--- playground/config/plugins.js
+++ playground/config/plugins.js
@@ -1,7 +1,25 @@
-module.exports = () => ({
-  "meilisearch": {
-    // Uncomment to use, some tests will fail
-    // "api_key": "masterKey",
-    // "host": "http://localhost:7700"
+module.exports = () => {
+  // use process.env.NODE_ENV and if conditions
+  // to create a custom config for each environment
+  // console.
+  const env = process.env.NODE_ENV || 'development'
+
+  if (env === 'test') {
+    return {}
   }
-})
+  else if (env === 'production') {
+    return {
+      meilisearch: {
+        apiKey: "masterKey",
+        host: "http://localhost:7700"
+      }
+    }
+  }
+  return {
+    meilisearch: {
+      apiKey: "masterKey",
+      host: "http://localhost:7700"
+    }
+  }
+}
+
