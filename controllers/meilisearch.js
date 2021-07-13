b25ad29ebb2b9a95e2c60a9b3a6f4e10731d817d
Mattias van den Belt <mattiasvandenbelt@gmail.com>
2021-07-13 13:46:55 +0200

    Update updateCollections to allow update from UI

--- controllers/meilisearch.js
+++ controllers/meilisearch.js
@@ -72,11 +72,13 @@ async function deleteIndex(ctx) {
 
 async function waitForDocumentsToBeIndexed(ctx) {
   const { indexUid } = ctx.params
+  const collection = strapi.config.plugins.meilisearch.collections.find(item => item.name === indexUid);
+
   const credentials = await getCredentials()
   const numberOfDocuments = await meilisearch
     .http(meilisearch.client(credentials))
     .waitForPendingUpdates({
-      indexUid,
+      indexUid: collection.index || collection.name,
       updateNbr: 2,
     })
   return { numberOfDocuments }
@@ -102,16 +104,18 @@ async function addCredentials(ctx) {
 }
 
 async function UpdateCollections(ctx) {
-  const { collection: indexUid } = ctx.params
+  let { collection } = ctx.params
+  collection = strapi.config.plugins.meilisearch.collections.find(item => item.name === collection);
+
   const credentials = await getCredentials()
   const { updateId } = await meilisearch
     .http(meilisearch.client(credentials))
     .deleteAllDocuments({
-      indexUid,
+      indexUid: collection.index || collection.name,
     })
   await meilisearch.http(meilisearch.client(credentials)).waitForPendingUpdate({
     updateId,
-    indexUid,
+    indexUid: collection.index || collection.name,
   })
   return addCollection(ctx)
 }
@@ -149,28 +153,40 @@ async function numberOfRowsInCollection({ collection }) {
 }
 
 async function batchAddCollection(ctx) {
-  const { collection } = ctx.params
-  const count = await numberOfRowsInCollection({ collection })
-  const BATCH_SIZE = 1000
-  const updateIds = []
-  for (let index = 0; index <= count; index += BATCH_SIZE) {
-    const rows = await fetchRowBatch({
-      start: index,
-      limit: BATCH_SIZE,
-      collection,
-    })
-    const { updateId } = await indexDocuments({ collection, documents: rows })
-    if (updateId) updateIds.push(updateId)
+  let { collection } = ctx.params
+  collection = strapi.config.plugins.meilisearch.collections.find(item => item.name === collection);
+  const index = collection.index || collection.name
+
+  for (const i in strapi.config.plugins.meilisearch.collections) {
+    const item = strapi.config.plugins.meilisearch.collections[i]
+
+    if(index === (item.index || item.name)){
+      console.log(item);
+      const count = await numberOfRowsInCollection({ collection: item.name })
+      const BATCH_SIZE = 1000
+      const updateIds = []
+      for (let index = 0; index <= count; index += BATCH_SIZE) {
+        const rows = await fetchRowBatch({
+          start: index,
+          limit: BATCH_SIZE,
+          collection: item.name,
+        })
+
+        const { updateId } = await indexDocuments({ collection: item.index || item.name, documents: rows.map(row => ({...row, id: item.name + row.id })) })
+        if (updateId) updateIds.push(updateId)
+      }
+    }
   }
-  return { updateIds }
 }
 
 async function addCollection(ctx) {
-  const { collection } = ctx.params
+  let { collection } = ctx.params
+  collection = strapi.config.plugins.meilisearch.collections.find(item => item.name === collection);
+
   const credentials = await getCredentials()
   // Create collection in MeiliSearch
   await meilisearch.http(meilisearch.client(credentials)).createIndex({
-    indexUid: collection,
+    indexUid: collection.index || collection.name,
   })
   batchAddCollection(ctx) // does not wait for add documents requests
   return { message: 'Index created' }
