5e64ed51daaca2d7e87747836a24adb0d1c5e38a
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-11-10 22:10:08 +0100

    Remove dead link

--- resources/custom-index-name/custom-index-name.js
+++ resources/custom-index-name/custom-index-name.js
@@ -1,10 +1,5 @@
 'use strict'
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
 module.exports = {
   meilisearch: {
     indexName: 'my_custom_index_name',
