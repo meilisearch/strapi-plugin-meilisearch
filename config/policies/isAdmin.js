ca2faaa1412591fdefcf1d2d7fd1b350f48d0a99
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-11-23 18:30:58 +0100

    Restrict plugin to only super admin users

--- config/policies/isAdmin.js
+++ config/policies/isAdmin.js
@@ -1,11 +1,9 @@
 'use strict'
 
 module.exports = async (ctx, next) => {
-  console.log('state', ctx.state.user)
   if (!ctx.state.user) {
     ctx.unauthorized('Access refused : Please be logged in.')
   }
-  console.log('role', ctx.state.user.roles)
   if (ctx.state.user.roles) {
     for (let i = 0; ctx.state.user.roles[i]; i++) {
       if (ctx.state.user.roles[i].code === 'strapi-super-admin') {
