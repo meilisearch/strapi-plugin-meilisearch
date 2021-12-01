d97be1c15cda1ac577a347205f8c4df3ee1fc287
cvermand <33010418+bidoubiwa@users.noreply.github.com>
2021-12-01 19:34:01 +0100

    Remove global strapi usage in controller (#7)

--- meilisearch/server/controllers/meilisearch.js
+++ meilisearch/server/controllers/meilisearch.js
@@ -1,12 +1,8 @@
 // Since v4, they created a global API containing all the functions I defined everywhere in my code.  Well, if you call the function in which you are … it creates an infinite loop
 'use strict'
 
-function getCollections(ctx) {
-  console.log('hello')
-
-  ctx.body = { message: 'hello world ' }
-}
-
-module.exports = {
-  getCollections,
-}
+module.exports = ({ strapi }) => ({
+  getCollections(ctx) {
+    ctx.body = { message: 'hello world ' }
+  },
+})
