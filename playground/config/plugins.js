2caff183f4bc80cdbcebf0b991ac21bb355217ae
Zoran Kokeza <zoran.kokeza@htecgroup.com>
2024-10-01 14:53:23 +0200

    upgrade playground to v5

--- playground/config/plugins.js
+++ playground/config/plugins.js
@@ -1,33 +1,3 @@
 const path = require('path')
 
-module.exports = ({ env }) => ({
-  meilisearch: {
-    enabled: true,
-    resolve: path.resolve(__dirname, '../src/plugins/meilisearch'),
-    config: {
-      'restaurant': {
-        filterEntry({ entry }) {
-          return entry.id !== 2
-        },
-        transformEntry({ entry }) {
-          return {
-            ...entry,
-            categories: entry.categories.map(category => category.name),
-          }
-        },
-        indexName: ['my_restaurant'],
-        settings: {
-          searchableAttributes: ['*'],
-        },
-      },
-      'about-us': {
-        indexName: ['content'],
-      },
-      'homepage': {
-        indexName: ['content'],
-      },
-      // host: "http://localhost:7700",
-      // apiKey: "masterKey"
-    },
-  },
-})
+module.exports = ({ env }) => ({})
