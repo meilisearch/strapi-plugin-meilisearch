a35076c351f7097112fda203d927667153954fc4
Zoran Kokeza <zoran.kokeza@htecgroup.com>
2024-10-02 09:05:57 +0200

    warking server reload

--- admin/src/pages/HomePage.jsx
+++ admin/src/pages/HomePage.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-import { BackButton, Layouts, Page } from '@strapi/strapi/admin'
+import {
+  BackButton,
+  Layouts,
+  Page,
+  private_AutoReloadOverlayBlockerProvider as AutoReloadOverlayBlockerProvider,
+} from '@strapi/strapi/admin'
 
 import { useI18n } from '../Hooks/useI18n'
 import PluginTabs from '../containers/PluginTabs'
@@ -9,19 +14,21 @@ const HomePage = () => {
   const { i18n } = useI18n()
 
   return (
-    <Page.Main>
-      <Layouts.Header
-        title={i18n('plugin.name', 'Meilisearch')}
-        subtitle={i18n(
-          'plugin.description',
-          'Search in your content-types with the Meilisearch plugin',
-        )}
-        navigationAction={<BackButton />}
-      />
-      <Layouts.Content>
-        <PluginTabs />
-      </Layouts.Content>
-    </Page.Main>
+    <AutoReloadOverlayBlockerProvider>
+      <Page.Main>
+        <Layouts.Header
+          title={i18n('plugin.name', 'Meilisearch')}
+          subtitle={i18n(
+            'plugin.description',
+            'Search in your content-types with the Meilisearch plugin',
+          )}
+          navigationAction={<BackButton />}
+        />
+        <Layouts.Content>
+          <PluginTabs />
+        </Layouts.Content>
+      </Page.Main>
+    </AutoReloadOverlayBlockerProvider>
   )
 }
 
