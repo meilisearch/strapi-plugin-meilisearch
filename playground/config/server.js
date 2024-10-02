76434b22d1191d6441f6848c6a33d37c1d7dd903
Zoran Kokeza <zoran.kokeza@htecgroup.com>
2024-10-02 17:52:23 +0200

    again 3

--- playground/config/server.js
+++ playground/config/server.js
@@ -2,9 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [
+      'ioiPGw5OvU/BWJNck6fTZw13wOo=',
+      'K82NUl4Vpz8XMivvgLusNxs0SRk=',
+    ]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-});
+})
